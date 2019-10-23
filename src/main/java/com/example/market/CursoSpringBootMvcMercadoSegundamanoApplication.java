efd852d45c2b372737916ead4127fd9b8ef54064
Iker Apaolaza Clemente <iapaolaza@indaba.lks.es>
2019-10-23 12:19:00 +0200

    First commit.

---
+++ src/main/java/com/example/market/CursoSpringBootMvcMercadoSegundamanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootMvcMercadoSegundamanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootMvcMercadoSegundamanoApplication.class, args);
+	}
+
+}
